ae3541b442629fcd8980d437f608186d45d9c611
Barak Saidoff <barak.saidoff@gmail.com>
2019-11-25 17:16:24 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/bloggers', to: 'bloggers#create'
+  get '/bloggers/new', to: 'bloggers#new', as: 'new_blogger'
+  get '/bloggers/:id', to: 'bloggers#show', as: 'blogger'
 end
